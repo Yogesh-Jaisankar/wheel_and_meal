a53d41238422ef28a95798688a8bc5a91f434fb8
YOGESH JAISANKAR <yogeshjaisnkar@gmail.com>
2024-09-16 21:11:52 +0530

    User page ui updated

--- lib/Screens/UserDetails.dart
+++ lib/Screens/UserDetails.dart
@@ -2,7 +2,9 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:mongo_dart/mongo_dart.dart' as mongo;
+import 'package:wheel_and_meal/Screens/home.dart';
 
 class UserDetailsPage extends StatefulWidget {
   final String phoneNumber; // Passed from the OTP page
@@ -93,6 +95,13 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('User details saved successfully!')),
         );
+
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => Home()), // Replace with your target page
+          (Route<dynamic> route) => false, // This clears all previous routes
+        );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Failed to save user details!')),
@@ -101,6 +110,36 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
     }
 
     await db.close();
+  }
+
+  bool validateInputs() {
+    if (nameController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your first name.')),
+      );
+      return false;
+    }
+    if (lastNameController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your last name.')),
+      );
+      return false;
+    }
+    if (selectedDate == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please select your date of birth.')),
+      );
+      return false;
+    }
+    if (ageController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                'Please calculate your age by selecting your date of birth.')),
+      );
+      return false;
+    }
+    return true;
   }
 
   @override
@@ -113,49 +152,161 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text('Enter Your Details')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: "First Name"),
-            ),
-            TextFormField(
-              controller: lastNameController,
-              decoration: InputDecoration(labelText: "Last Name"),
-            ),
-            TextFormField(
-              controller: phoneController,
-              enabled: false, // Phone number is not editable
-              decoration: InputDecoration(labelText: "Phone Number"),
-            ),
-            GestureDetector(
-              onTap: () => _selectDate(context),
-              child: AbsorbPointer(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: selectedDate == null
-                        ? "Date of Birth"
-                        : DateFormat('yyyy-MM-dd').format(selectedDate!),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Lottie.asset("assets/user.json", height: 200, width: 200),
+                SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black87),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                            controller: nameController,
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Colors.black87),
+                                hintText: "First Name",
+                                border: InputBorder.none),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black87,
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black87),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                            controller: lastNameController,
+                            decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Colors.black87),
+                                hintText: "Last Name",
+                                border: InputBorder.none),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black87),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: TextStyle(
+                          color: Colors.black87, fontWeight: FontWeight.bold),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        labelStyle: TextStyle(color: Colors.black87),
+                      ),
+                      controller: phoneController,
+                      enabled: false, // Phone number is not editable
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(height: 25),
+                GestureDetector(
+                  onTap: () => _selectDate(context),
+                  child: AbsorbPointer(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black87),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                            border: InputBorder.none,
+                            labelText: selectedDate == null
+                                ? "Date of Birth"
+                                : DateFormat('yyyy-MM-dd')
+                                    .format(selectedDate!),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black87),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.black87),
+                      controller: ageController,
+                      enabled: false, // Age is auto-calculated
+                      decoration: InputDecoration(
+                          labelStyle: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold),
+                          labelText: "Age",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () async {
+                      if (validateInputs()) {
+                        await storeUserData();
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "Continue",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            TextFormField(
-              controller: ageController,
-              enabled: false, // Age is auto-calculated
-              decoration: InputDecoration(labelText: "Age"),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await storeUserData();
-              },
-              child: Text('Submit'),
-            ),
-          ],
+          ),
         ),
       ),
     );
